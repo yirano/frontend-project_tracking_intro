b7b7822debcc4f986c449c23318d8f7fabbb64d8
yirano <yirano@protonmail.ch>
2019-10-24 18:05:34 -0400

    Animate hamburger menu

--- src/script.js
+++ src/script.js
@@ -1,7 +1,13 @@
 let collapse = document.querySelector('.collapse-menu')
 let nav = document.querySelector('nav ul')
+let firstBar = document.querySelector('.one')
+let secondBar = document.querySelector('.two')
+let thirdBar = document.querySelector('.three')
 
 collapse.addEventListener('click', function(){
   nav.classList.toggle('hide')
-  console.log('clicked');
+  secondBar.classList.toggle('two-animate');
+  firstBar.classList.toggle('one-animate');
+  thirdBar.classList.toggle('hide');
 })
+
